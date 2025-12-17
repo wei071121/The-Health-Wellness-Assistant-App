307ff29116e11c932207ed9119ed6a2c40bc8b72
wei071121 <jadenwong1121@gmail.com>
2025-12-18 00:00:26 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,13 +1,7 @@
-import tkinter as tk
-
-def clear_window(root):
-    """Clear all widgets from the window"""
-    for widget in root.winfo_children():
-        widget.destroy()
-
-def darken_color(hex_color, percent):
-    """Darken a hex color by given percentage"""
-    hex_color = hex_color.lstrip('#')
-    rgb = tuple(int(hex_color[i:i+2], 16) for i in (0, 2, 4))
-    darkened = tuple(max(0, int(c * (1 - percent/100))) for c in rgb)
-    return '#%02x%02x%02x' % darkened
+def is_valid_time(t):
+    if len(t) != 5 or t[2] != ":":
+        return False
+    hh, mm = t.split(":")
+    if not (hh.isdigit() and mm.isdigit()):
+        return False
+    return 0 <= int(hh) < 24 and 0 <= int(mm) < 60
