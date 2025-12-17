584b0d4023fc24518e22757aeeb88447cc074cea
wei071121 <jadenwong1121@gmail.com>
2025-12-18 00:04:12 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,7 +1,10 @@
 def is_valid_time(t):
-    if len(t) != 5 or t[2] != ":":
+    if len(t) != 5 or t[2] != ':':
         return False
-    hh, mm = t.split(":")
+    hh, mm = t.split(':')
     if not (hh.isdigit() and mm.isdigit()):
         return False
-    return 0 <= int(hh) < 24 and 0 <= int(mm) < 60
+    hh = int(hh)
+    mm = int(mm)
+    return 0 <= hh < 24 and 0 <= mm < 60
+
